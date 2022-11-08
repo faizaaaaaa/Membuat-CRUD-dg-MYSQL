c9a1029ede4113994af2ad333bb1e50fa4f5c117
PPTQ Quantum Qolbu <fitriyaninurfaizah467@gmail.com>
2022-11-08 15:31:33 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+var config = {
+	database: {
+		// host:	  'localhost', 		// database host
+		host:	  'localhost', 		// database host
+		user: 	  'root', 			// your database username
+		password: '', 			// your database password
+		port: 	  3306, 			// default MySQL port
+		db: 	  't-countries' // your database name
+	},
+	server: {
+		host:'localhost', // the host for the server
+		port: '3000' // the port for the server
+	}
+}
+
+module.exports = config
